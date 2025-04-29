958c45b415ae2de94c682f095b2aecf97bfb29d9
Bashar Umar <basharu83@gmail.com>
2025-04-29 12:49:25 +0100

    added auth pages

---
+++ src/views/auth/AuthView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="row">
+      <router-view />
+    </div>
+  </div>
+</template>
