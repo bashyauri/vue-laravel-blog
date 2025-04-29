e7a6cfde205174aec974f66f12460f6c5715b44e
Bashar Umar <basharu83@gmail.com>
2025-04-29 13:03:47 +0100

    added styling to login and register

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>
-    <h1>Register Page</h1>
+  <div class="row">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h2 align="center">Register</h2>
+            <form action="">
+              <div class="form-group">
+                <label for="name">Name</label>
+                <input
+                  id="name"
+                  class="form-control form-control-lg"
+                  type="text"
+                  placeholder="Enter your name"
+                />
+              </div>
+
+              <div class="form-group mt-3">
+                <label for="email">Email</label>
+                <input
+                  id="email"
+                  class="form-control form-control-lg"
+                  type="email"
+                  placeholder="Enter your email"
+                />
+              </div>
+
+              <div class="form-group mt-3">
+                <label for="password">Password</label>
+                <input
+                  id="password"
+                  class="form-control form-control-lg"
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+
+              <br />
+
+              <a href="/login">Login into your account</a>
+
+              <br /><br />
+
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-lg w-100">Register</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
   </div>
 </template>
